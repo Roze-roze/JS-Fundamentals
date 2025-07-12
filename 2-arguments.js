e5623c2312c9eef3ad8122fef417d2b6ebde376d
Roze-roze <kassimroze@gmail.com>
2025-07-12 19:55:42 +0300

    Arguments

---
+++ 2-arguments.js
@@ -0,0 +1,24 @@
+
+if(arg===0){
+    console.log("No argument");
+}
+else if(arg===1){
+    console.log("Argument found");
+}
+else{
+    console.log("Arguments found")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
